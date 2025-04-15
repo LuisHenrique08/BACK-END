b781fd3373020b86a2f1b7c58d21e496e51b0703
LuisHenrique08 <162449796+LuisHenrique08@users.noreply.github.com>
2025-04-15 11:59:03 -0300

    Add files via upload

---
+++ Trabalho.js
@@ -0,0 +1,36 @@
+let times = require("./dados.json");
+
+
+function verTimes(){
+    times.forEach((time) =>{
+        console.log (time.nome + "-" + time.goleiro + "(goleiro)," + time.alaDireita + "(Ala Direita)," + time.alaEsquerda + "(Ala Esquerda)," + time.pivo + "(Pivo)," + time.fixo + "(Fixo)," + time.tecnico + "(Técnico)," + time.vitoria + "(Vitórias)," + time.derrota + "(Derrotas),")
+    });
+}
+verTimes();
+
+function criarArquivo(){
+    let dadosEmtexto = JSON.stringify(times);
+    console.log(dadosEmtexto);
+    const fs = require("fs");
+    fs.writeFileSync("dados.json", dadosEmtexto);
+    console.log("Arquivo gerado com sucesso!")
+}
+
+function marcarJogos(timeA, timeB, data, horario, estadio, cidade){
+    let jogos = require("./jogos.json");
+    jogo = {
+        timeA: timeA,
+        timeB: timeB,
+        data: data,
+        horario: horario,
+        estadio: estadio,
+        cidade: cidade,
+    }
+    jogos.push(jogo);
+    jogosJSON = JSON.stringify(jogos);
+    const fs=require('fs');
+    fs.writeFileSync
+
+}
+//criarArquivo();
+verTimes();
